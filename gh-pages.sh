3275a75a71a8b684401d49758f68015a7b36c4e9
Alex Rupérez <me@alexruperez.com>
2020-04-16 16:27:23 +0200

    Jekyll fix.

--- gh-pages.sh
+++ gh-pages.sh
@@ -2,9 +2,9 @@ git remote set-url origin git@github.com:alexruperez/SecurePropertyStorage.git
 git checkout -B gh-pages
 gem install jazzy
 ./jazzy.sh
-find . -type f ! -path "./.git/*" ! -path "./docs*" ! -path "./LICENSE" -exec rm -f {} +
-rm -f docs/.build
+find . -type f ! -path "./.git/*" ! -path "./.build/*" ! -path "./docs*" ! -path "./LICENSE" -exec rm -f {} +
 mv docs/* ./
+touch .nojekyll
 git add -f .
 git commit -m "Documentation update."
 git push -f origin gh-pages
