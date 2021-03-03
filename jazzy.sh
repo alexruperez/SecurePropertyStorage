2fb723a818257e3901420cefc008feabf9ef916e
Alex Rupérez <alejandro.ruperez@globant.com>
2021-03-03 13:10:43 +0100

    Fixed documentation.

--- jazzy.sh
+++ jazzy.sh
@@ -1,7 +1,7 @@
 mkdir -p docs
-sourcekitten doc --spm-module Storage > docs/Storage.json
-sourcekitten doc --spm-module Keychain > docs/Keychain.json
-sourcekitten doc --spm-module UserDefault > docs/UserDefault.json
-sourcekitten doc --spm-module Singleton > docs/Singleton.json
-sourcekitten doc --spm-module Inject > docs/Inject.json
+sourcekitten doc --spm --module-name Storage > docs/Storage.json
+sourcekitten doc --spm --module-name Keychain > docs/Keychain.json
+sourcekitten doc --spm --module-name UserDefault > docs/UserDefault.json
+sourcekitten doc --spm --module-name Singleton > docs/Singleton.json
+sourcekitten doc --spm --module-name Inject > docs/Inject.json
 jazzy --config .jazzy.yml
