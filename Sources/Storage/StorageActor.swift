7faa4f02fa576d205470637e205d0efad5fa6981
Alex Rupérez <me@alexruperez.com>
2025-09-02 15:42:35 +0200

    Fix(storage): make StorageActor.shared immutable (#41)
    Static property 'shared' is not concurrency-safe because it is non-isolated global shared mutable state.
    
    Making the 'shared' property a 'let' instead of a 'var' ensures that it is immutable and thus concurrency-safe. This resolves the build error that occurs on Xcode 16 Beta 7.

--- Sources/Storage/StorageActor.swift
+++ Sources/Storage/StorageActor.swift
@@ -1,5 +1,5 @@
 /// A global actor responsible for managing access and write operations in storage systems.
 @globalActor
 public actor StorageActor {
-    public static var shared = StorageActor()
+    public static let shared = StorageActor()
 }
